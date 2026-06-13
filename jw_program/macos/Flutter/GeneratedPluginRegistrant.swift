6ee14e92aa7f1006fd9095c14c33d81474361c76
VicentCodes <vicentenevarezt@gmail.com>
2026-06-13 12:35:33 -0600

    chore(deps): update dependencies and platform configs

--- jw_program/macos/Flutter/GeneratedPluginRegistrant.swift
+++ jw_program/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import file_selector_macos
+import flutter_secure_storage_darwin
+import share_plus
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
